294519359b4d85d8efdd560001aafbc4b24d2122
Michael Jordan <michael@glowingpixel.com>
2023-02-11 15:16:11 -0800

    Clumsy first implementation of export

--- gui.py
+++ gui.py
@@ -4,6 +4,30 @@ import sys, pathlib, typing
 import locatorator
 
 MARKER_COMMENT_COLUMN_NAME = "Shot ID"
+EXPORT_TRACK_OPTIONS = ("TC1","V1","V2","V3","V4","V5","V6","V7","V8")
+EXPORT_DEFAULT_MARKER_NAME = "Locatorator"
+EXPORT_DEFAULT_MARKER_COLOR = "white"
+
+class MarkerIcons:
+
+	sig_new_icon_created = QtCore.Signal(str)
+	icons = {}
+
+	@classmethod
+	def prepare_icon(cls, marker_color:str):
+		"""Draw marker icon"""
+		
+		pm = QtGui.QPixmap(64, 64)
+		pm.fill(QtGui.QColor(0,0,0,0))
+
+		painter = QtGui.QPainter(pm)
+		color   = QtGui.QColor(marker_color)
+		painter.setBrush(QtGui.QBrush(color))
+		painter.drawEllipse(0, 0, pm.width(), pm.height())
+		painter.end()
+
+		cls.icons[marker_color] = QtGui.QIcon(pm.scaledToHeight(6, QtCore.Qt.TransformationMode.SmoothTransformation))
+	#	cls.sig_new_icon_created.emit(marker_color)
 
 class MarkerViewer(QtWidgets.QTreeWidget):
 
@@ -17,34 +41,15 @@ class MarkerViewer(QtWidgets.QTreeWidget):
 			"Offset"
 		]
 
-		self._icons = {}
-
 		self._setup()
 	
 	def _setup(self):
 
-		self._prepare_icons()
 		self.setIndentation(0)
 		self.setHeaderLabels(self._headerlabels)
 		self.setAlternatingRowColors(True)
 		self.setUniformRowHeights(True)
 		self.setSortingEnabled(True)
-	
-	def _prepare_icons(self):
-		"""Draw marker icons"""
-
-		for marker_color in (m.lower() for m in locatorator.MarkerColors._member_names_):
-			pm = QtGui.QPixmap(64, 64)
-			pm.fill(QtGui.QColor(0,0,0,0))
-
-			painter = QtGui.QPainter(pm)
-			color   = QtGui.QColor(marker_color)
-			painter.setBrush(QtGui.QBrush(color))
-			painter.drawEllipse(0, 0, pm.width(), pm.height())
-			painter.end()
-
-			self._icons[marker_color] = QtGui.QIcon(pm.scaledToHeight(6, QtCore.Qt.TransformationMode.SmoothTransformation))
-
 
 	def set_changelist(self, markers_changes:typing.Iterable[typing.Tuple[locatorator.Marker, locatorator.Marker, timecode.Timecode]]) -> None:
 
@@ -81,7 +86,7 @@ class MarkerViewer(QtWidgets.QTreeWidget):
 				if header != MARKER_COMMENT_COLUMN_NAME:
 					changelist_item.setTextAlignment(idx, QtCore.Qt.AlignmentFlag.AlignRight|QtCore.Qt.AlignmentFlag.AlignCenter)
 			
-			changelist_item.setIcon(0, self._icons.get(marker_new.color.name.lower() if marker_new else marker_old.name.lower(), "red"))
+			changelist_item.setIcon(0, MarkerIcons.icons.get(marker_new.color.name.lower() if marker_new else marker_old.name.lower(), "red"))
 			
 			changelist.append(changelist_item)
 
@@ -92,6 +97,58 @@ class MarkerViewer(QtWidgets.QTreeWidget):
 				self.resizeColumnToContents(idx)
 
 		self.sortByColumn(self._headerlabels.index("New TC"), QtCore.Qt.SortOrder.AscendingOrder)
+
+class OutputFileGroup(QtWidgets.QGroupBox):
+
+	sig_export_requested = QtCore.Signal(str, locatorator.MarkerColors, str, str)
+	sig_export_canceled = QtCore.Signal()
+	sig_export_complete = QtCore.Signal(str)
+
+	def __init__(self):
+		super().__init__()
+
+		self._layout = QtWidgets.QHBoxLayout()
+		self._cmb_color = QtWidgets.QComboBox()
+		self._cmb_track = QtWidgets.QComboBox()
+		self._btn_export = QtWidgets.QPushButton()
+		self._txt_name = QtWidgets.QLineEdit()
+	
+		self._setup()
+
+	def _setup(self):
+
+		self.setLayout(self._layout)
+		
+		for color in MarkerIcons.icons:
+			self._cmb_color.addItem(MarkerIcons.icons.get(color),"",color)
+		
+		# TODO: Oh boy test this
+		self._cmb_color.setCurrentIndex(list(MarkerIcons.icons.keys()).index(EXPORT_DEFAULT_MARKER_COLOR))
+		self.layout().addWidget(self._cmb_color)
+
+		for track in EXPORT_TRACK_OPTIONS:
+			self._cmb_track.addItem(track)
+		self.layout().addWidget(self._cmb_track)
+
+		self._txt_name.setText(EXPORT_DEFAULT_MARKER_NAME)
+		self.layout().addWidget(self._txt_name)
+
+		self._btn_export.setText("Export Marker List...") 
+		self.layout().addWidget(self._btn_export)
+
+		self._btn_export.clicked.connect(self._export_markers)
+	
+	@QtCore.Slot()
+	def _export_markers(self, markers:typing.Iterable[locatorator.Marker]) -> None:
+		"""Export a given marker list"""
+
+		path_output = QtWidgets.QFileDialog.getSaveFileName(self, "Choose a location to save your markers", filter="Marker Lists (*.txt);;All Files (*)")[0]
+		
+		if not path_output:
+			self.sig_export_canceled.emit()
+			return
+		
+		self.sig_export_requested.emit(path_output, locatorator.MarkerColors(self._cmb_color.currentData()), self._cmb_track.currentText(), self._txt_name.text())
 
 class InputFileChooser(QtWidgets.QWidget):
 
@@ -191,24 +248,66 @@ class MainWidget(QtWidgets.QWidget):
 
 	def __init__(self):
 		super().__init__()
+		self._prep_marker_icons()
 
 		self._layout = QtWidgets.QVBoxLayout()
 		self._grp_list_inputs = InputListGroup()
 		self._tree_viewer = MarkerViewer()
+		self._exporter = OutputFileGroup()
 
 		self._locked = False
 		self._path_old = pathlib.Path()
 		self._path_new = pathlib.Path()
 
+		self._markerlist = []
+
 		self._setup()
 	
 	def _setup(self):
 
+
 		self.setLayout(self._layout)
 		self.layout().addWidget(self._grp_list_inputs)
 		self.layout().addWidget(self._tree_viewer)
+		self.layout().addWidget(self._exporter)
 
 		self._grp_list_inputs.sig_paths_chosen.connect(self._set_paths)
+
+		self._exporter.sig_export_requested.connect(self._save_marker_list)
+	
+	@QtCore.Slot()
+	def _save_marker_list(self, path_output:str, marker_color:locatorator.MarkerColors=locatorator.MarkerColors.WHITE, marker_track:str="TC1", marker_name:str=EXPORT_DEFAULT_MARKER_NAME):
+
+		with open(path_output, "w") as file_output:
+			for marker_old, marker_new, tc_delta in self._markerlist:
+				if marker_old and marker_new and tc_delta:
+					marker_output = locatorator.Marker(
+						name=marker_name,
+						color=marker_color,
+						tc_start=str(marker_new.timecode.start),
+						duration=1,
+						track=marker_track,
+						comment=f"Cut change near {marker_new.comment}: {tc_delta} ({marker_old.timecode.start} -> {marker_new.timecode.start})"
+					)
+				elif not marker_old:
+					marker_output = locatorator.Marker(
+						name=marker_name,
+						color=marker_color,
+						tc_start=str(marker_new.timecode.start),
+						duration=1,
+						track=marker_track,
+						comment=f"Shot added: {marker_new.comment}"
+					)
+				else:
+					marker_output = locatorator.Marker(
+						name=marker_name,
+						color=marker_color,
+						tc_start=str(marker_old.timecode.start),
+						duration=1,
+						comment=f"Shot removed: {marker_old.comment}"
+					)
+				print(marker_output, file=file_output)
+		
 	
 	def _set_paths(self, path_old:str, path_new:str):
 
@@ -221,9 +320,14 @@ class MainWidget(QtWidgets.QWidget):
 		with self._path_new.open() as file_new:
 			markers_new = locatorator.get_marker_list_from_file(file_new)
 		
-		markers_changes = locatorator.build_marker_changes(markers_old, markers_new)
+		self._markerlist = locatorator.build_marker_changes(markers_old, markers_new)
 
-		self._tree_viewer.set_changelist(markers_changes)
+		self._tree_viewer.set_changelist(self._markerlist)
+	
+	def _prep_marker_icons(self):
+		"""Prepare marker icons based on Marker Colors"""
+		for marker_color in (m.lower() for m in locatorator.MarkerColors._member_names_):
+			MarkerIcons.prepare_icon(marker_color)
 	
 class MainWindow(QtWidgets.QMainWindow):
 	 
