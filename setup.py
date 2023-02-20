47d02752d3cc880e50dc2e094b4440376975c536
Michael Jordan <michael@glowingpixel.com>
2023-02-19 20:13:43 -0800

    Packaging things

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ setup(
 	name="locatorator",
 	version="1.0.0",
 	packages=["locatorator"],
-	install_requires=["posttools @ git+https://github.com/mjiggidy/posttools.git#egg=posttools"],
+	install_requires=["posttools @ git+https://github.com/mjiggidy/posttools.git#egg=posttools","PySide6"],
 	entry_points={
 		"console_scripts":[
-			"locatorator = locatorator.__main__:bootstrap"
+			"locatorator_cli = locatorator.__main__:bootstrap"
+		],
+        "gui_scripts":[
+			"locatorator = locatorator.gui:main"
 		]
 	}
 )
