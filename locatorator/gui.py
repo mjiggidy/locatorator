8e23945f9f6a5190877486ccf92af34e9543c5e3
Michael Jordan <michael@glowingpixel.com>
2026-02-18 16:08:42 -0800

    Version bump

--- locatorator/gui.py
+++ locatorator/gui.py
@@ -568,7 +568,7 @@ def main() -> int:
 	app = QtWidgets.QApplication(sys.argv)
 	app.setOrganizationName("GlowingPixel")
 	app.setApplicationName("Locatorator")
-	app.setApplicationVersion("1.2.0")
+	app.setApplicationVersion("1.3.0")
 
 	app.setWindowIcon(QtGui.QPixmap(":/icons/resources/icon.png"))
 
